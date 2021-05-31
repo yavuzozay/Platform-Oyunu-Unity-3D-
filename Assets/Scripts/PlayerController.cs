2e5cee057beaa4ab0386441fb8e48ab687552d37
yavuzozay <57958423+yavuzozay@users.noreply.github.com>
2021-05-31 14:20:01 +0300

    assets edit
    Animations and Scripts added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,16 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
+
+
 {
-    // Start is called before the first frame update
+    CharacterController characterController;
+    [SerializeField] float speed;
+    [SerializeField] float jump;
+    [SerializeField] float verticalSpeed;
+    [SerializeField] float horizontalSpeed;
+    private float groundDistance = 0.1f;
+    private float gravity = -9.81f;
+    float mSpeedY = 0;
+
     void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Move();
+
+    }
+
+    private void Move()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = 1;
+        Vector3 movement = new Vector3(0, 0, z).normalized + new Vector3(x * horizontalSpeed, 0, 0);
+
+        Vector3 verticalMovement = Vector3.up * mSpeedY * Time.deltaTime;
+
+
+        if (!characterController.isGrounded)
+            mSpeedY += gravity * Time.deltaTime;
+        else
+            mSpeedY = 0;
+        characterController.Move(verticalMovement + (movement * Time.deltaTime));
     }
 }
